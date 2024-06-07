685406be5de40885c7e6e9d9f55db09cc22d7d06
Samuel Ramos <samuelaoliveiraramos@gmail.com>
2024-06-07 19:58:07 -0300

    chore: Add options file for NextAuth configuration

---
+++ src/lib/nextAuth/options/index.ts
@@ -0,0 +1,56 @@
+import {
+  GetServerSidePropsContext,
+  NextApiRequest,
+  NextApiResponse,
+} from "next";
+import { AuthOptions, getServerSession } from "next-auth";
+
+import {
+  AGE_SESSION,
+  COOKIE_SESSION_NAME,
+  UPDATE_AGE_SESSION,
+  secretKey,
+} from "@/constants";
+import { getCustomProvider, jwtCallback, sessionCallback } from "../auth";
+
+export const authOptions: AuthOptions = {
+  secret: secretKey,
+  /**
+   * TODO - Add your preferred provider
+   */
+  providers: [getCustomProvider()],
+
+  callbacks: {
+    jwt: jwtCallback,
+    session: sessionCallback,
+  },
+
+  cookies: {
+    sessionToken: {
+      name: COOKIE_SESSION_NAME,
+      options: {
+        httpOnly: true,
+        sameSite: "lax",
+        path: "/",
+        secure: process.env.NODE_ENV === "production",
+      },
+    },
+  },
+
+  session: {
+    strategy: "jwt",
+    maxAge: AGE_SESSION,
+    updateAge: UPDATE_AGE_SESSION,
+  },
+};
+
+function auth(
+  ...args:
+    | [GetServerSidePropsContext["req"], GetServerSidePropsContext["res"]]
+    | [NextApiRequest, NextApiResponse]
+    | []
+) {
+  return getServerSession(...args, authOptions);
+}
+
+export { auth };
