d09c9375698b7e12b13a6a534531c9ab1ea7b587
Samuel Ramos <samuelaoliveiraramos@gmail.com>
2024-06-10 12:52:07 -0300

    chore: Update public API route to fetch data from Rick and Morty API

--- src/app/api/public/route.ts
+++ src/app/api/public/route.ts
@@ -1,9 +1,47 @@
+import { FETCH_OPTIONS } from "@/enum";
+import { fetchAPI } from "@/service";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(req: NextRequest) {
-  return new NextResponse(JSON.stringify({ message: "Hello, World!" }), {
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
+/**
+ * @name GET
+ * @description
+ * This function is a route handler for the public API endpoint.
+ * It fetches data from the Rick and Morty API and returns the data.
+ * @param {NextRequest} _
+ * @returns {Promise<NextResponse>}
+ 
+ */
+export async function GET(_: NextRequest) {
+  try {
+    const response = await fetchAPI({
+      baseURL: "https://rickandmortyapi.com/api/character",
+      method: FETCH_OPTIONS.GET,
+      path: "",
+    });
+
+    if (response.error) {
+      return NextResponse.json(
+        {
+          message: response.error,
+        },
+        {
+          status: 500,
+        }
+      );
+    }
+
+    return NextResponse.json(response.data, {
+      status: 200,
+    });
+  } catch (error) {}
+
+  return NextResponse.json(
+    { message: "Hello, World!" },
+    {
+      status: 200,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  );
 }
