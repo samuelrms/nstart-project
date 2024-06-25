9417f758c2f10dd52c428ade4536bf07bcf68139
Samuel Ramos <samuelaoliveiraramos@gmail.com>
2024-06-25 12:38:54 -0300

    feat: Add CustomInput and CustomSelect components
    The code changes in this commit add the CustomInput and CustomSelect components to the components directory. These components provide custom input and select functionality, allowing for more flexibility in form design and user interaction.

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,7 @@
 export * from "./ActiveLink";
 export * from "./Breadcrumb";
+export * from "./CustomInput";
+export * from "./CustomSelect";
 export * from "./OTP";
 export * from "./OTP/enum";
 export * from "./Stack";
