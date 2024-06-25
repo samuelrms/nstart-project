b769fb93016826062092e132ba3fcbecdf0c842d
Samuel Ramos <samuelaoliveiraramos@gmail.com>
2024-06-25 12:39:09 -0300

    chore: Refactor Stack component to remove unnecessary code
    The code changes in this commit refactor the Stack component by removing the unnecessary assignment of the elementType variable. This variable is no longer used and can be safely removed, simplifying the code and improving readability.

--- src/components/Stack/index.tsx
+++ src/components/Stack/index.tsx
@@ -13,7 +13,7 @@ export const Stack = ({
   className,
   ...props
 }: StackProps) => {
-  const Element = elementType || "div";
+  const Element = elementType;
 
   const classNameProps = twMerge("flex", className);
 
