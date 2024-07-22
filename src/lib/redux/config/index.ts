3a638214222feb9c064933876405f417c518dfa8
Samuel Ramos <samuelaoliveiraramos@gmail.com>
2024-07-22 16:00:15 -0300

    refactor: Update import statements and export all items in LIB

--- src/lib/redux/config/index.ts
+++ src/lib/redux/config/index.ts
@@ -2,6 +2,9 @@ import { configureStore } from "@reduxjs/toolkit";
 import { middleware } from "../middleware";
 import { reducer } from "../reducer";
 
+/**
+ * ANCHOR - Configuration template, don't change it if you don't know what you're doing
+ */
 export const config = configureStore({
   reducer,
   middleware,
